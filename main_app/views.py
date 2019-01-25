c0adb3f37c0e10ba22ee0c67503a2b07844aa260
vindi <vrathnayake@gmail.com>
2019-01-24 19:45:48 -0800

    fixed error in pycharm

--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from .models import Treasure
 
 
-
 def index(request):
     treasures = Treasure.objects.all()
     return render(request, 'index.html', {'treasures': treasures})
-    
